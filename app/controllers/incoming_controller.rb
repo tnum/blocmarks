689fe79aaf319ba9bb822f3682ee59005527e24b
tnum <t.chudha@gmail.com>
2015-04-27 17:43:51 +0100

    incoming controller params updated

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,11 +5,11 @@ class IncomingController < ApplicationController
     puts "INCOMING PARAMS HERE: #{params}"
 
     @user = User.find_by(email: params[:sender])
+    @url = params["body-plain"]
 
     if @user.present?
-      @topic = @user.topics.find_or_create_by(title: params[:subject])
-      @url = params["body-plain"]
-      @bookmark = Bookmark.create(url: @url)
+      @topic = @user.topics.find_or_create_by(title: params[:subject], user: @user)
+      @bookmark = Bookmark.new(topic: @topic, url: @url)
     end
 
     # @topic.bookmarks.create(url: @url)
