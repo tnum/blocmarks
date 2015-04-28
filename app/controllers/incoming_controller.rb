06458a1d0cbeedd962619a7b60e5bb446363a704
tnum <teenum@susumu.co.uk>
2015-04-28 21:58:59 +0100

    incoming controller improved and cleaned up

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,19 +2,16 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    puts "INCOMING PARAMS HERE: #{params}"
 
-    @user = User.find_by(email: params[:sender])
+    user = User.find_by(email: params[:sender])
 
     if @user.present?
-      @topic = Topic.find_or_create_by(user_id: @user.id, title: params[:subject])
-      @topic.save
-      @url = params["body-plain"]
-      @bookmark = Bookmark.create(topic: @topic, url: @url)
-      @bookmark.save
+      topic = Topic.find_or_create_by(user: user, title: params[:subject])
+      url = params["body-plain"]
+      bookmark = Bookmark.create(topic: topic, url: url)
     end
 
-    head 200
+    head :ok
   end
 
 end
