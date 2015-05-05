6cd486225dc479537b744c64dc10bf5b9b740e3a
Teenum Applegate <teenum@Teenums-iMac.local>
2015-05-05 17:33:54 +0100

    bookmark likes working with correct db structure

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :topic
   has_one :user, through: :topic
+  has_many :likes, dependent: :destroy
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504214015) do
+ActiveRecord::Schema.define(version: 20150505160433) do
 
   create_table "bookmarks", force: true do |t|
     t.string   "url"
